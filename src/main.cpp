a4c9e723b057fdacacb253b8ab0d4a903980ed14
sangpham2710 <tansang.27.10.2003@gmail.com>
2022-11-29 00:42:28 +0700

    add TGUI into CMakeLists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,26 @@
-#include <SFML/Graphics.hpp>
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// TGUI - Texus' Graphical User Interface
+// Copyright (C) 2012-2022 Bruno Van de Velde (vdv_b@tgui.eu)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+#include <TGUI/TGUI.hpp>
+#include <TGUI/Backend/SFML-Graphics.hpp>
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
-    return 0;
+bool runExample(tgui::BackendGui& gui)
+{
+    return true;
 }
+
+int main()
+{
+    sf::RenderWindow window{ {800, 600}, "TGUI example - SFML_GRAPHICS backend" };
+
+    tgui::Gui gui{window};
+    if (runExample(gui))
+        gui.mainLoop();
+}// subject to the following restrictions:
