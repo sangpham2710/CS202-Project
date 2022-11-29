b972c5cdf09f21434d0624b18d540b3e3b3cb784
NhutThi2404 <nhutth2404@gmail.com>
2022-11-29 14:49:45 +0700

    Display grass path

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,49 @@
 #include "Application.h"
 
 int main() {
-    try {
-        Application app;
-        app.run();
-    } catch (std::exception& e) {
-        std::cout << "\nEXCEPTION: " << e.what() << std::endl;
+  sf::RenderWindow window(sf::VideoMode({1800, 900}), "SFML Project");
+  sf::Event event;
+
+   sf::Texture textureGrassPath1;
+  textureGrassPath1.loadFromFile(
+      "D:/The University of Science/Semester "
+      "4/CS202/Project/assets/textures/background/grass-path-1.png");
+  sf::Sprite spriteGrassPath1(textureGrassPath1);
+
+  sf::Texture textureGrassPath2;
+  textureGrassPath2.loadFromFile(
+      "D:/The University of Science/Semester "
+      "4/CS202/Project/assets/textures/background/grass-path-3.png");
+  sf::Sprite spriteGrassPath2(textureGrassPath2);
+
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) window.close();
     }
+
+    window.clear();
+    int widthMax = 1800;
+    int heightMax = 900;
+    int width = 0;
+    int height = 0;
+    int countBlock = 0;
+    while (width < widthMax && height < heightMax) {
+      height = 0;
+
+      if (countBlock % 3 == 0) {
+        spriteGrassPath1.setPosition(width, height);
+        window.draw(spriteGrassPath1);
+      } else {
+        spriteGrassPath2.setPosition(width, height);
+        window.draw(spriteGrassPath2);
+      }
+      height += 64;
+
+      countBlock++;
+      width += 64;
+    }
+    window.display();
+  }
     return 0;
 }
