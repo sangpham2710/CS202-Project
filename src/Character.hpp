53486efc0836c655bc82aa8cd515bbe25ed3660f
qplong21 <100032289+qplong21@users.noreply.github.com>
2022-11-29 16:41:11 +0700

    Declare Character Class

--- src/Character.hpp
+++ src/Character.hpp
@@ -1,1 +1,18 @@
 #pragma once
+
+class Character
+{
+public:
+	Character(float x, float y);
+	void drawTo(sf::RenderWindow& window);
+	void processEvents(sf::Keyboard::Key key, bool checkPressed);
+	void update();
+	void changeTexture(int type);
+	int getPhaseTime();
+
+private:
+	sf::Clock clock;
+	sf::RectangleShape hitBox;
+	sf::Texture texture;
+	bool left, up, down, right;
+};
