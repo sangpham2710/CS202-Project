b868d87ed1a9879456eb6b82d16e9803d974445c
sangpham2710 <tansang.27.10.2003@gmail.com>
2022-11-29 23:04:23 +0700

    add main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <stdexcept>
 
-#include "Application.h"
+#include "Application.hpp"
 
 int main() {
     try {
@@ -13,31 +12,3 @@ int main() {
     }
     return 0;
 }
-=======
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-// TGUI - Texus' Graphical User Interface
-// Copyright (C) 2012-2022 Bruno Van de Velde (vdv_b@tgui.eu)
-//
-// This software is provided 'as-is', without any express or implied warranty.
-// In no event will the authors be held liable for any damages arising from the use of this software.
-//
-// Permission is granted to anyone to use this software for any purpose,
-// including commercial applications, and to alter it and redistribute it freely,
-#include <TGUI/TGUI.hpp>
-#include <TGUI/Backend/SFML-Graphics.hpp>
-
-bool runExample(tgui::BackendGui& gui)
-{
-    return true;
-}
-
-int main()
-{
-    sf::RenderWindow window{ {800, 600}, "TGUI example - SFML_GRAPHICS backend" };
-
-    tgui::Gui gui{window};
-    if (runExample(gui))
-        gui.mainLoop();
-}// subject to the following restrictions:
->>>>>>> tgui
