adc34fe00a4eaef21e4bc16637b16c9873f3df55
NhutThi2404 <nhutth2404@gmail.com>
2022-11-29 15:09:45 +0700

    Display river

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include "Application.h"
 
 int main() {
-  sf::RenderWindow window(sf::VideoMode({1800, 900}), "SFML Project");
+  sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML Project");
   sf::Event event;
 
    sf::Texture textureGrassPath1;
@@ -19,6 +19,19 @@ int main() {
       "4/CS202/Project/assets/textures/background/grass-path-3.png");
   sf::Sprite spriteGrassPath2(textureGrassPath2);
 
+  sf::Texture textureBush;
+  textureBush.loadFromFile(
+      "D:/The University of Science/Semester "
+      "4/CS202/Project/assets/textures/background/bush-1.png");
+  sf::Sprite spriteBush(textureBush);
+
+  sf::Texture textureRiver;
+  textureRiver.loadFromFile(
+      "D:/The University of Science/Semester "
+      "4/CS202/Project/assets/textures/background/river-1.png");
+  sf::Sprite spriteRiver(textureRiver);
+
+
   while (window.isOpen()) {
     sf::Event event;
     while (window.pollEvent(event)) {
@@ -26,8 +39,8 @@ int main() {
     }
 
     window.clear();
-    int widthMax = 1800;
-    int heightMax = 900;
+    int widthMax = 800;
+    int heightMax = 600;
     int width = 0;
     int height = 0;
     int countBlock = 0;
@@ -35,12 +48,18 @@ int main() {
       height = 0;
 
       if (countBlock % 3 == 0) {
-        spriteGrassPath1.setPosition(width, height);
-        window.draw(spriteGrassPath1);
-      } else {
         spriteGrassPath2.setPosition(width, height);
         window.draw(spriteGrassPath2);
+        spriteBush.setPosition(width, height);
+        window.draw(spriteBush);
+      } else {
+        spriteGrassPath1.setPosition(width, height);
+        window.draw(spriteGrassPath1);
       }
+      height += 64;
+
+      spriteRiver.setPosition(width, height);
+      window.draw(spriteRiver);
       height += 64;
 
       countBlock++;
