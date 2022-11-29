edc64faf9eabb02b18b1bb2b0733c95d64fe5c43
ngvahieu <vanhieu230303@gmail.com>
2022-11-29 14:21:35 +0700

    add Obstacle class

---
+++ src/Obstacle.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Entity.h"
+
+class Obstacle : public Entity {
+	public:
+		Obstacle();
+
+		bool isMoving();
+		int getDirection();
+		void setDirection();
+		
+		void move(sf::Vector2f delta);
+
+		~Obstacle();
+	private:
+		bool moving; // true: animal, vehicle / false: water,fire-pump 
+		int direction; // 0: left, 1: right
+};
