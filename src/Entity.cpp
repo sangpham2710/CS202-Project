d2d89c20386357a9856f9df88f8a2033a3cca333
sangpham2710 <tansang.27.10.2003@gmail.com>
2022-11-29 22:58:35 +0700

    add Entity class

---
+++ src/Entity.cpp
@@ -0,0 +1,33 @@
+#include "Entity.hpp"
+
+#include <cassert>
+
+
+Entity::Entity() : mVelocity() {
+}
+
+void Entity::setVelocity(sf::Vector2f velocity) {
+    mVelocity = velocity;
+}
+
+void Entity::setVelocity(float vx, float vy) {
+    mVelocity.x = vx;
+    mVelocity.y = vy;
+}
+
+sf::Vector2f Entity::getVelocity() const {
+    return mVelocity;
+}
+
+void Entity::accelerate(sf::Vector2f velocity) {
+    mVelocity += velocity;
+}
+
+void Entity::accelerate(float vx, float vy) {
+    mVelocity.x += vx;
+    mVelocity.y += vy;
+}
+
+void Entity::updateCurrent(sf::Time dt, CommandQueue&) {
+    move(mVelocity * dt.asSeconds());
+}
