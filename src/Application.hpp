93e1f7020d4ad93373ff47eda21b2027cd769b2b
sangpham2710 <tansang.27.10.2003@gmail.com>
2022-11-29 22:52:09 +0700

    add Application class

---
+++ src/Application.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Text.hpp>
+#include <SFML/System/Time.hpp>
+
+#include "Player.hpp"
+#include "ResourceHolder.hpp"
+#include "ResourceIdentifiers.hpp"
+#include "StateStack.hpp"
+
+
+class Application {
+   public:
+    Application();
+    void run();
+
+
+   private:
+    void processInput();
+    void update(sf::Time dt);
+    void render();
+
+    void updateStatistics(sf::Time dt);
+    void registerStates();
+
+
+   private:
+    static const sf::Time TimePerFrame;
+
+    sf::RenderWindow mWindow;
+    TextureHolder mTextures;
+    FontHolder mFonts;
+    Player mPlayer;
+
+    StateStack mStateStack;
+
+    sf::Text mStatisticsText;
+    sf::Time mStatisticsUpdateTime;
+    std::size_t mStatisticsNumFrames;
+};
