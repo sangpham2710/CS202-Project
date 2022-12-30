2cc20b117ef7c2fb5ebbd1e8dde366031efc6219
minhvip08 <minhvip08@gmail.com>
2022-12-30 15:47:42 +0700

    Press buttons

--- src/MenuState.cpp
+++ src/MenuState.cpp
@@ -33,10 +33,28 @@ MenuState::MenuState(StateStack& stack, Context context)
         mCommandQueue.push(command);
     };
 
+    auto playButtonClickSound = [&] {
+        Command command;
+        command.category = Category::SoundEffect;
+        command.action =
+            derivedAction<SoundNode>([&](SoundNode& node, sf::Time) {
+            node.playSound(SoundEffect::ButtonClick,
+                { 0.5 * Constants::SCREEN_WIDTH,
+                 0.5 * Constants::SCREEN_HEIGHT });
+                });
+        mCommandQueue.push(command);
+    };
+
     playButton->onMouseEnter(playButtonHoverSound);
+    playButton->onPress(playButtonClickSound);
     loadButton->onMouseEnter(playButtonHoverSound);
+    loadButton->onPress(playButtonClickSound);
+
     settingsButton->onMouseEnter(playButtonHoverSound);
+    settingsButton->onPress(playButtonClickSound);
     exitButton->onMouseEnter(playButtonHoverSound);
+    exitButton->onPress(playButtonClickSound);
+
 
     alignCenter(menuLabel, window);
 
